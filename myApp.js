bbbe1ff8af53b4e2b07db2e47d58c7f89dda2152
Thiagouh <139012325+Thiagouh@users.noreply.github.com>
2024-10-22 23:06:34 -0300

    initial commit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,35 @@
-let express = require('express');
+require("dotenv").config();
+
+let express = require("express");
 let app = express();
 
+app.get('/now', function actualTime(req, res, next) {
+  
+})
 
+app.use(function createRegistry(req, res, next) {
+  let string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
+  next();
+});
 
+console.log("Hello World");
 
+app.get("/", function (req, res) {
+  //res.send('Hello Express');
+  absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", function (req, res) {
+  let response = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: response.toUpperCase() });
+  } else {
+    res.json({ message: response });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
